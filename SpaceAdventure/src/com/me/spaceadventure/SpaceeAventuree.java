aaec51ae4b1d4c49a1806f42db7d6229b3f89b44
Susan <Susan@10.42.0.216>
2012-11-19 13:55:03 -0500

    my commit

--- SpaceAdventure/src/com/me/spaceadventure/SpaceeAventuree.java
+++ SpaceAdventure/src/com/me/spaceadventure/SpaceeAventuree.java
@@ -32,6 +32,7 @@ public class SpaceeAventuree implements ApplicationListener {
 		sprite.setOrigin(sprite.getWidth()*0, sprite.getHeight()*0);
 		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
 		//asdsdsjd
+		//gjgjggj
 	}
 
 	@Override
